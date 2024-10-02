4e7f28bf342ab2d6fc53046ec6fe8ca0af27a621
David Woolf <davidjohnwoolf@gmail.com>
2024-10-02 09:28:17 -0600

    cleanup

--- src/taskpane/hooks/useLoadDefinitionsEffect.ts
+++ src/taskpane/hooks/useLoadDefinitionsEffect.ts
@@ -3,16 +3,16 @@ import { Dispatch, SetStateAction, useEffect } from "react";
 import { reduceParagraphsToDefinitions } from "../utils/reduceParagraphsToDefinitions";
 import { ValueById } from "../types";
 
-/** @todo to handle the doc change, probably pass the state in and watch it, then set defs */
+/**
+ * Loads definitions into context on mount
+ * @todo to handle the document change, probably pass the state in and watch it, then set defs
+ */
 export const useLoadDefinitionsEffect = (setDefinitions: Dispatch<SetStateAction<ValueById>>) => {
   useEffect(() => {
-    /** Load the document definitions on mount */
     (async () => {
       await Word.run(async ({ document, sync }) => {
         const paragraphs = document.body.paragraphs;
-        // document.onParagraphChanged.add(async ({ uniqueLocalIds }) => {
-        //   const isDefinitionChange = uniqueLocalIds.some((id) => currentIds.includes(id));
-        // });
+        // document.onParagraphChanged.add(() => alert("You just done messed up"));
         paragraphs.load("text, uniqueLocalId");
         await sync();
 
