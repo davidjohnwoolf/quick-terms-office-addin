9ea40b065965492831b4b637dd4430769c1b2e42
David Woolf <davidjohnwoolf@gmail.com>
2024-10-02 09:10:27 -0600

    cleanup

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,27 +1,20 @@
 import * as React from "react";
 import DefinitionList from "./DefinitionList";
-import { makeStyles } from "@fluentui/react-components";
 import Header from "./Header";
 import { useSelectionChangeEffect } from "../hooks/useSelectionChangeEffect";
-
-const useClasses = makeStyles({
-  app: {},
-});
 
 /** Layout and election state managed here and in the custom effect hook */
 const App: React.FC = () => {
   const [currentSelection, setCurrentSelection] = React.useState<string>();
   const [showAll, setShowAll] = React.useState<boolean>(false);
 
-  const classes = useClasses();
-
   useSelectionChangeEffect(setCurrentSelection);
 
   return (
-    <section className={classes.app}>
+    <main>
       <Header onShowAll={(value) => setShowAll(value)} showAll={showAll} />
       <DefinitionList selection={!showAll && currentSelection} />
-    </section>
+    </main>
   );
 };
 
